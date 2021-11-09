ced7259e4bcaa18a81663aca1d4d2e4c1ae8ad9d
ikemanairing <ikemanairing@gmail.com>
2021-11-09 20:49:06 +0900

    memo function with Cat Component

---
+++ Practice7/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, memo } from "react";
+
+const Cat = memo(({ name, meow = (f) => f }) => {
+  console.log(`rendering ${name}`);
+
+  return <p onClick={() => meow(name)}> {name} </p>;
+});
+
+const PureCat = memo(
+  Cat,
+  (prevProps, nextProps) => prevProps.name !== nextProps.name
+);
+
+export default function App() {
+  const [cats, setCats] = useState(["Biscuit", "Jungle", "Outlaw"]);
+  return (
+    <>
+      {cats.map((name, i) => (
+        <PureCat
+          key={i}
+          name={name}
+          meow={(name) => console.log(`${name} has meowed`)}
+        />
+      ))}
+      <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
+        Add a Cat
+      </button>
+    </>
+  );
+}
